926808369466c710d1478004dd4f8423250c13b6
ilknur Aydin <36745106+aydini@users.noreply.github.com>
2023-01-30 13:34:58 -0500

    Update Main.java

--- 01302023Lecture/Main.java
+++ 01302023Lecture/Main.java
@@ -1,1 +1,25 @@
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aydin.classwork;
+
+/**
+ *
+ * @author itlabs
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        double PI = 3.14159;
+        System.out.printf("****%10.3f****\n", PI);
+        System.out.printf("****%-10.3f****\n", PI);
+        System.out.printf("%10sMike%n%-10sMike%n%20s%n%-20s",
+                                           "hello1",
+                                           "hello2",
+                                           "hello3",
+                                           "hello4");
+    }
+}
