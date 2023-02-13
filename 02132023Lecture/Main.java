a62510e3abe1c6644db46ebbadbd999a71c7a431
ilknur Aydin <36745106+aydini@users.noreply.github.com>
2023-02-13 13:24:08 -0500

    Add files via upload

---
+++ 02132023Lecture/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aydin._classwork;
+
+/**
+ *
+ * @author itlabs
+ */
+public class Main {
+    public static void main(String[] args) {
+        MathFunctions mf = new MathFunctions();
+        
+        mf.displayDate(2, 13, 2023, "/");
+        mf.displayDate("February", 13, 2023);
+        
+        
+    }   
+}
