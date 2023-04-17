76bdb15132cef633a05506a2d6ebcb78f386a6fc
ilknur Aydin <36745106+aydini@users.noreply.github.com>
2023-04-17 13:32:23 -0400

    Add files via upload

---
+++ 04172023/AbstractClassExample/Animal.java
@@ -0,0 +1,8 @@
+package aydin._abstractclassexample;
+
+
+// Animal is an abstract class
+public abstract class Animal {
+    public abstract void move(); // an abstract method
+    
+}
---
+++ 04172023/AbstractClassExample/Frog.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aydin._abstractclassexample;
+
+
+// Frog is a concrete class
+public class Frog extends Animal{ 
+    @Override
+    public void move(){ // example of a concrete method
+        System.out.println("Frog jumps");
+    }
+}
