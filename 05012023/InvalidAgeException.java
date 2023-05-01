6d61751842afcd753d8c55048881e1c2880537b2
ilknur Aydin <36745106+aydini@users.noreply.github.com>
2023-05-01 13:34:33 -0400

    Create InvalidAgeException.java

---
+++ 05012023/InvalidAgeException.java
@@ -0,0 +1,12 @@
+
+package com.mycompany._classwork;
+
+
+public class InvalidAgeException extends Exception {
+    
+    public InvalidAgeException(String msg){
+        super(msg);
+    }
+    
+}
+ 
