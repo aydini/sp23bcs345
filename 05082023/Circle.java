c923577d780043ec79e6396725e75121a3c7231e
ilknur Aydin <36745106+aydini@users.noreply.github.com>
2023-05-08 13:35:29 -0400

    Add files via upload

---
+++ 05082023/Circle.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany._classwork;
+
+/**
+ *
+ * @author itlabs
+ */
+public class Circle extends TwoDimensionalShape{
+
+@Override
+public void getArea(){
+     System.out.println("calculating aread of Circle...");
+}
+
+}
